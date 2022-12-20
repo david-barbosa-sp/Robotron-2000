f23397d7f1fc4a244c9a74d0c0ab7c90070a0abc
David Barbosa <davidbarbosa@outlook.com>
2022-12-20 12:33:15 -0300

    Melhorando o main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
-const robotron = document.querySelector('#robotron');
 
-robotron.addEventListener('click', (evento) => {
-    console.log(evento);
-});
 
-function dizOi(nome){
-    console.log('oi!' + nome);
-    console.log('Bem vindo ao robotron 2000');
+const controle = document.querySelectorAll('[data-controle]');
+controle.forEach((elemento)=>{
+    elemento.addEventListener('click', (e)=>{manipulaDados(e.target.dataset.controle, e.target.parentNode)})
+   
 }
 
-dizOi('David');
+)
+
+function manipulaDados(operacao, controle){    
+    
+    const peca = controle.querySelector('[data-contador]')
+    
+    if(operacao === '-'){    
+     peca.value = parseInt(peca.value) - 1;
+    }
+    else{
+     peca.value = parseInt(peca.value) + 1;
+    }
+}
